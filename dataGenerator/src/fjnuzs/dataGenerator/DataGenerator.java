7382c1369ebf168d6f02676c1a67a111002e67d9
fjnuzs <shi@fjnu.edu.cn>
2017-04-14 16:22:42 +0800

    start

---
+++ dataGenerator/src/fjnuzs/dataGenerator/DataGenerator.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package fjnuzs.dataGenerator;
+
+/**
+ * @author zhang
+ *
+ */
+public class DataGenerator {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
